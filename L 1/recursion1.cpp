5784490c502a28cfc2e47cce6bf6d8315bec65f3
Abhinav <62789397+ImBatman-7@users.noreply.github.com>
2022-09-09 01:48:32 +0530

    Add files via upload

--- L 1/recursion1.cpp
+++ L 1/recursion1.cpp
@@ -1,1 +1,24 @@
+//print numbers in reverse order
 
+#include <iostream>
+using namespace std;
+void rev(int n) {
+	//base case
+	if (n < 1) {
+		return;
+	}
+	cout << n << " ";
+	rev(n - 1);
+}
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	rev(n);
+
+	return 0;
+}
